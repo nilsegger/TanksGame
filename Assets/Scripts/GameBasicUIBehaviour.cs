55deccea3fc129bc4c6f3e66410a0c5dfc15f58a
Nils <nilsxegger@gmail.com>
2022-02-26 10:58:16 +0100

    measuring ping

--- Assets/Scripts/GameBasicUIBehaviour.cs
+++ Assets/Scripts/GameBasicUIBehaviour.cs
@@ -8,8 +8,14 @@ public class GameBasicUIBehaviour : NetworkBehaviour
 {
 
     public Text m_CountdownText;
+    public float m_UpdatePingEveryS = 1.0f;
 
     private float _countdown = 0.0f;
+
+    private Dictionary<int, double> _pingsSendTime = new Dictionary<int, double>();
+    private int _nextPingId = 0;
+    private float _pingCooldown = 0.0f;
+    
 
     private IEnumerator WaitToBeginCountdown(double waitTime, float countdownTime)
     {
@@ -40,5 +46,45 @@ public class GameBasicUIBehaviour : NetworkBehaviour
         {
             m_CountdownText.gameObject.SetActive(false);
         }
+
+        if (GameManagerBehaviour.GameBegun)
+        {
+            if (_pingCooldown <= 0.0f)
+            {
+                SendPingRequest();
+                _pingCooldown = m_UpdatePingEveryS;
+            }
+            else
+            {
+                _pingCooldown -= Time.deltaTime;
+            }
+        }
+    }
+    
+    private void SendPingRequest()
+    {
+        _pingsSendTime[_nextPingId] = NetworkManager.LocalTime.Time;
+        PingRequestServerRpc(_nextPingId);
+        _nextPingId++;
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void PingRequestServerRpc(int pingId, ServerRpcParams serverRpcParams = default)
+    {
+        ClientRpcParams clientRpcParams = new ClientRpcParams
+        {
+            Send = new ClientRpcSendParams
+            {
+               TargetClientIds = new ulong[]{serverRpcParams.Receive.SenderClientId}
+            }
+        };
+        PingResponseClientRpc(pingId, clientRpcParams);
+    }
+
+    [ClientRpc]
+    private void PingResponseClientRpc(int pingId, ClientRpcParams clientRpcParams = default)
+    {
+        Debug.Log(NetworkManager.LocalTime.Time - _pingsSendTime[pingId]);
+        _pingsSendTime.Remove(pingId);
     }
 }
