deb71324c1b4e807b320522c3f53d5a73cb45842
Nils <nilsxegger@gmail.com>
2022-03-03 13:04:39 +0100

    fixed tankvisibility and fog values

--- Assets/Scripts/Tank/TankVisibility.cs
+++ Assets/Scripts/Tank/TankVisibility.cs
@@ -9,7 +9,7 @@ using Vector3 = UnityEngine.Vector3;
 public class TankVisibility : NetworkBehaviour
 {
 
-    public Transform m_Turret;
+    public Transform m_Eyes;
     
     public float m_VisibilityRange = 15.0f;
     public float m_AngleRange = 180.0f;
@@ -41,7 +41,7 @@ public class TankVisibility : NetworkBehaviour
         Vector2 xzOther = new Vector2(other.transform.position.x, other.transform.position.z);
         var otherForward = xzOther - xzPlayer;
 
-        var p1 = new Vector2(m_Turret.forward.x, m_Turret.forward.z) * otherForward.magnitude;
+        var p1 = new Vector2(m_Eyes.forward.x, m_Eyes.forward.z) * otherForward.magnitude;
         var p2 = otherForward;
 
         return (Mathf.Atan2(p1.y, p1.x) - Mathf.Atan2(p2.y, p2.x)) * Mathf.Rad2Deg;
@@ -61,7 +61,7 @@ public class TankVisibility : NetworkBehaviour
              var degreesBetween = CalculateDegreesBetweenPlayerSightAndOther(playerObject, otherObject);
              inDistance = -m_AngleRange / 2.0f <= degreesBetween && degreesBetween <= m_AngleRange / 2.0f;
              
-             Debug.DrawLine(m_Turret.position, m_Turret.position + (m_Turret.rotation * Quaternion.Euler(0.0f, degreesBetween, 0.0f) * Vector3.forward) * m_VisibilityRange, inDistance ? Color.green : Color.red);
+             Debug.DrawLine(m_Eyes.position, m_Eyes.position + (m_Eyes.rotation * Quaternion.Euler(0.0f, degreesBetween, 0.0f) * Vector3.forward) * m_VisibilityRange, inDistance ? Color.green : Color.red);
          }
 
          bool isObserver = false;
