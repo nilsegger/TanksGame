02f63d8f685730b822a7294b48c7d48971bbe1ec
Nils <nilsxegger@gmail.com>
2022-03-05 18:04:26 +0100

    MobileStartup Scene

---
+++ Assets/Scripts/PlayfabBehaviour.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayfabBehaviour : MonoBehaviour
+{
+
+    public Text m_InfoText;
+
+    private void SignInUsingDeviceId()
+    {
+
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            PlayFabClientAPI.LoginWithAndroidDeviceID(new LoginWithAndroidDeviceIDRequest(),
+                result => { CheckIfServerOnline(); }, error =>
+                {
+                    m_InfoText.text = "Failed to authenticate device.";
+                    Debug.Log(error.GenerateErrorReport());
+                });
+        } else if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            var request = new LoginWithCustomIDRequest
+            {
+                CustomId = "test",
+                CreateAccount = true 
+            };
+            
+            PlayFabClientAPI.LoginWithCustomID(request, result => { CheckIfServerOnline(); },
+                error =>
+                {
+                    m_InfoText.text = "Failed to authenticate device.";
+                    Debug.Log(error.GenerateErrorReport());
+                });
+        }
+    }
+   
+    private void CheckIfServerOnline() {
+        PlayFabClientAPI.GetTitleData(new GetTitleDataRequest(),
+            result => {
+                if (result.Data == null || !result.Data.ContainsKey("ServersOnline"))
+                {
+                    m_InfoText.text = "Received faulty info from servers.";
+                }
+                else if (result.Data["ServersOnline"].Equals("false"))
+                {
+                    m_InfoText.text = "Servers are offline.";
+                }
+                else
+                {
+                    LoadMainMenu();
+                }
+            },
+            error =>
+            {
+                m_InfoText.text = "Failed to fetch data.";
+                Debug.Log(error.GenerateErrorReport());
+            }
+        );
+    }
+
+    private void LoadMainMenu()
+    {
+        m_InfoText.text = "Now i would load lobby";
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_InfoText.text = "Checking Server Status...";
+       SignInUsingDeviceId(); 
+    }
+    
+}
